28bbb93a9072de4962ba00ca21cb47f884cecb20
Moun2201 <ahmounfc@gmail.com>
2024-07-20 11:29:41 +0700

    test datatype

--- testdatatype.py
+++ testdatatype.py
@@ -1,7 +1,22 @@
-str="Hello world"
-print(str)
-print(str[0])
-print(str[2:5])
-print(str[2:])
-print(str*10)
-print(str + "from BBU BTB")
+# str="Hello world"
+# print(str)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str*10)
+# print(str + "from BBU BTB")
+
+#number
+# a=1_000_000
+# print(a)
+# print(type(a))
+# b=13.04
+# print(type(b))
+# print(b)
+
+val1=int("10")
+val2=int("20")
+total=val1+val2
+print(type(total))
+print("Total"+ str(total))
+print(len(str(total)))
